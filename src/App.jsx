0953d4a6d28dcc37341225484364f892d9e40615
Rajan <krajan92946@gmail.com>
2024-11-11 22:28:01 +0530

    App addTask woking

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoRed 
+import { useState } from "react"
+import { useDispatch } from "react-redux";
+
+const App=()=>{
+  const [txtval,setTxtval]=useState("")
+  const [edtBtn,setEdbtn]=useState(true);
+  const [edId, setEdID]=useState("")
+  const mydata=useState((state)=>state.todo.task)
+  const mydis=useDispatch();
+  
+  console.log(mydata)
+
+  let sno=0;
+  const delTask=(id)=>{
+    mydis(recDelete(id))
+  }
+}
