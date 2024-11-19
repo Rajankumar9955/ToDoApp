89416b683979706143de73966b8bc77a9d90dd2c
Rajan <krajan92946@gmail.com>
2024-11-19 23:40:44 +0530

    App section add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
 import { useState } from "react"
 import { useDispatch } from "react-redux";
 
-const App=()=>
+const App=()=>{
+  const[txtval,setTxtval]=useState("");
+  const[edBtn,secti]
+  return(
+    <>
+    <h1>To Do APP</h1>
+    Enter Your Task: <input type="text" value={txtval} onChange={(e)=>{setTxtval(e.target.value)}} />
+
+     <button onClick={mydis(addTask({id:Date.now(),data:txtval,status:false}))}>ADD</button>
+     <hr size="4" color="black" />
+     <table>
+      <tr>
+        <th>Sno</th>
+        <th>Your Task</th>
+        <th>Delete</th>
+        <th>Status</th>
+        <th>Edit</th>
+      </tr>
+      {ans}
+     </table>
+    </>
+  )
+}
+export default App;
